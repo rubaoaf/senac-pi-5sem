3848a5128775aeff22f7b467a2fdcb4f3a6e96c3
KarinaSegundo <kahsegundo@hotmail.com>
2022-11-06 19:11:20 -0300

    Alteração das fontes

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
+// import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
+import { useFonts, Dosis_400Regular } from '@expo-google-fonts/dosis';
 
 import Routes from './src/router';
 
 export default function App() {
 
   let [fontsLoaded] = useFonts({
-    Anton_400Regular,
+    // Anton_400Regular,
+    Dosis_400Regular,
   });
 
   if(!fontsLoaded){
